105383d46bbdcff6852df24325eb1959ea3ca0fc
karine-2022 <116926092+karine-2022@users.noreply.github.com>
2022-10-31 20:49:11 -0300

    Add files via upload

---
+++ exercicio_for.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+
+int main (void){
+
+    int x;
+    
+    for (x=0;x<10; x++)
+    
+    printf("posicao de x ->%d \n",x);
+    
+    
+      
+     system("pause");
+    return 0 ;
+}
